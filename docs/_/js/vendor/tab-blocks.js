94bbd62556d60b7b993fcfb01c9ffd1bcacc46dd
openapiprocessor <info@openapiprocessor.io>
2021-05-30 08:19:25 +0000

    regenerated docs

--- docs/_/js/vendor/tab-blocks.js
+++ docs/_/js/vendor/tab-blocks.js
@@ -1,1 +1,50 @@
-!function(){"use strict";var t=window.location.hash;function a(t){var a=this.tab,e=this.pane;i(".tabs li, .tab-pane",this.tabset).forEach((function(t){t===a||t===e?t.classList.add("is-active"):t.classList.remove("is-active")})),t.preventDefault()}function i(t,a){return Array.prototype.slice.call((a||document).querySelectorAll(t))}i(".tabset").forEach((function(e){var s,c,n=e.querySelector(".tabs");n&&(i("li",n).forEach((function(n,r){var l=(n.querySelector("a[id]")||n).id;if(l){var o=function(t,a){return i(".tab-pane",a).find((function(a){return a.getAttribute("aria-labelledby")===t}))}(l,e);r||(c={tab:n,pane:o}),!s&&t==="#"+l&&(s=!0)?(n.classList.add("is-active"),o&&o.classList.add("is-active")):r||(n.classList.remove("is-active"),o&&o.classList.remove("is-active")),n.addEventListener("click",a.bind({tabset:e,tab:n,pane:o}))}})),!s&&c&&(c.tab.classList.add("is-active"),c.pane&&c.pane.classList.add("is-active")));e.classList.remove("is-loading")}))}();
+;(function () {
+  'use strict'
+
+  var hash = window.location.hash
+  find('.tabset').forEach(function (tabset) {
+    var active
+    var tabs = tabset.querySelector('.tabs')
+    if (tabs) {
+      var first
+      find('li', tabs).forEach(function (tab, idx) {
+        var id = (tab.querySelector('a[id]') || tab).id
+        if (!id) return
+        var pane = getPane(id, tabset)
+        if (!idx) first = { tab: tab, pane: pane }
+        if (!active && hash === '#' + id && (active = true)) {
+          tab.classList.add('is-active')
+          if (pane) pane.classList.add('is-active')
+        } else if (!idx) {
+          tab.classList.remove('is-active')
+          if (pane) pane.classList.remove('is-active')
+        }
+        tab.addEventListener('click', activateTab.bind({ tabset: tabset, tab: tab, pane: pane }))
+      })
+      if (!active && first) {
+        first.tab.classList.add('is-active')
+        if (first.pane) first.pane.classList.add('is-active')
+      }
+    }
+    tabset.classList.remove('is-loading')
+  })
+
+  function activateTab (e) {
+    var tab = this.tab
+    var pane = this.pane
+    find('.tabs li, .tab-pane', this.tabset).forEach(function (it) {
+      it === tab || it === pane ? it.classList.add('is-active') : it.classList.remove('is-active')
+    })
+    e.preventDefault()
+  }
+
+  function find (selector, from) {
+    return Array.prototype.slice.call((from || document).querySelectorAll(selector))
+  }
+
+  function getPane (id, tabset) {
+    return find('.tab-pane', tabset).find(function (it) {
+      return it.getAttribute('aria-labelledby') === id
+    })
+  }
+})()
