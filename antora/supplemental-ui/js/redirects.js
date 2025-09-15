770e5ad83a4f239e1a02e06858627847bba35f09
Martin Hauner <martin.hauner@gmx.net>
2025-09-15 18:32:17 +0200

    improve redirects

--- antora/supplemental-ui/js/redirects.js
+++ antora/supplemental-ui/js/redirects.js
@@ -1,17 +1,22 @@
 const redirects = [
-  { current: '/spring/2022.5', target: '/spring' },
-  { current: '/spring/2022.4', target: '/spring' },
-  { current: '/spring/2022.3', target: '/spring' },
-  { current: '/spring/2022.2', target: '/spring' },
-  { current: '/spring/2022.1', target: '/spring' }
+    /* spring */
+    {current: /(\/spring\/2023\.[12])/, target: '/spring'},
+    {current: /(\/spring\/2022\.\d)/, target: '/spring'},
+    {current: /(\/spring\/2021\.\d)/, target: '/spring'},
+    {current: /(\/spring\/2020\.\d)/, target: '/spring'},
+    /* micronaut */
+    {current: /(\/micronaut\/2023\.[12])/, target: '/micronaut'},
+    {current: /(\/micronaut\/2022\.\d)/, target: '/micronaut'},
+    {current: /(\/micronaut\/2021\.\d)/, target: '/micronaut'},
+    {current: /(\/micronaut\/2020\.\d)/, target: '/micronaut'},
 ];
 
 document.addEventListener('DOMContentLoaded', () => {
     const currentPath = window.location.pathname;
 
     for (const version of redirects) {
-        const pos = currentPath.indexOf(version.current);
-        if (pos > -1) {
+        const match = currentPath.match(version.current);
+        if (match.index != null) {
           const element = document.getElementById('redirect');
           element.textContent = 'This looks like a link to an old version, trying to redirect to the latest version.';
 
